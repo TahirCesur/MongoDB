ecf53f3ec1c51d068f97787fc3fe5de54a812d27
Tahir Cesur <delenkar2825@gmail.com>
2021-11-21 23:39:11 +0300

    F01

--- index..js
+++ index..js
@@ -20,78 +20,78 @@ app.get("/mongo", (req, res) => res.send(calısıncaMongoSonuc));
 //4 tane metod oluşturduk ve metodda oluşan sonuçları calısıncaMongoSonuc e atadık
 // 1)Create / Insert
 const insertDocuments = function (db, callback) {
-  // Koleksiyon, db objesinden alınır
-  const collection = db.collection("employees");
+    // Koleksiyon, db objesinden alınır
+    const collection = db.collection("employees");
 
-  // Koleksiyona çoklu ekleme işlemi
-  collection.insertMany(
-    [
-      { id: 1, adi: "Ahmet", soyadi: "Çınar", yas: 25 },
-      { id: 2, adi: "Caner", soyadi: "Sarı", yas: 30 },
-      { id: 3, adi: "Gülçin", soyadi: "Yılmaz", yas: 22 },
-    ],
-    function (err, docs) {
-      if (err) throw err;
+    // Koleksiyona çoklu ekleme işlemi
+    collection.insertMany(
+        [
+            {id: 1, adi: "Ahmet", soyadi: "Çınar", yas: 25},
+            {id: 2, adi: "Caner", soyadi: "Sarı", yas: 30},
+            {id: 3, adi: "Gülçin", soyadi: "Yılmaz", yas: 22},
+        ],
+        function (err, docs) {
+            if (err) throw err;
 
-      console.log("Koleksiyona 3 döküman eklenmiştir.");
-      calısıncaMongoSonuc = docs;
-      callback(docs);
-    }
-  );
+            console.log("Koleksiyona 3 döküman eklenmiştir.");
+            calısıncaMongoSonuc = docs;
+            callback(docs);
+        }
+    );
 };
 
 // 2) Read / Search
 const findDocuments = function (db, callback) {
-  // Koleksiyon, db objesinden alınır
-  //const collection = db.collection("employees");
+    // Koleksiyon, db objesinden alınır
+    //const collection = db.collection("employees");
 
-  // Id'si 3 olan kayıtı arama işlemi için: collection.find({ id: 3})
-  // Tüm kayıtları listelemek: collection.find({}) şeklinde kullanabilirsiniz.
-  db.collection("employees")
-    .find({})
-    .toArray(function (err, docs) {
-      if (err) throw err;
+    // Id'si 3 olan kayıtı arama işlemi için: collection.find({ id: 3})
+    // Tüm kayıtları listelemek: collection.find({}) şeklinde kullanabilirsiniz.
+    db.collection("employees")
+        .find({})
+        .toArray(function (err, docs) {
+            if (err) throw err;
 
-      console.log("Şu kayıtlar bulunmuştur;");
+            console.log("Şu kayıtlar bulunmuştur;");
 
-      console.log(docs);
-      calısıncaMongoSonuc = docs;
-      callback(docs);
-    });
+            console.log(docs);
+            calısıncaMongoSonuc = docs;
+            callback(docs);
+        });
 };
 
 // 3) Update
 const updateDocument = function (db, callback) {
-  // Koleksiyon, db objesinden alınır
-  const collection = db.collection("employees");
+    // Koleksiyon, db objesinden alınır
+    const collection = db.collection("employees");
 
-  // İlk parametre ile güncellenecek kayıt bulunur, sonra istenilen field'ı güncellenir.
-  collection.updateOne(
-    { id: 1 },
-    { $set: { yas: 26 } },
-    function (err, result) {
-      if (err) throw err;
+    // İlk parametre ile güncellenecek kayıt bulunur, sonra istenilen field'ı güncellenir.
+    collection.updateOne(
+        {id: 1},
+        {$set: {yas: 26}},
+        function (err, result) {
+            if (err) throw err;
 
-      console.log("1 Id'li kaydın yaşı güncellenmiştir.");
+            console.log("1 Id'li kaydın yaşı güncellenmiştir.");
 
-      callback(result);
-    }
-  );
+            callback(result);
+        }
+    );
 };
 
 //4) Delete
 const deleteDocument = function (db, callback) {
-  // Koleksiyon, db objesinden alınır
-  const collection = db.collection("employees");
+    // Koleksiyon, db objesinden alınır
+    const collection = db.collection("employees");
 
-  // Silme işlemi id alanı üzerinden yapılır
-  collection.deleteOne({ id: 2 }, function (err, result) {
-    if (err) throw err;
+    // Silme işlemi id alanı üzerinden yapılır
+    collection.deleteOne({id: 2}, function (err, result) {
+        if (err) throw err;
 
-    console.log("Id'si 2 olan kayıt silinmiştir.");
+        console.log("Id'si 2 olan kayıt silinmiştir.");
 
-    callback(result);
-  });
+        callback(result);
+    });
 };
 
 /// https://www.w3schools.com/nodejs/nodejs_mongodb_createcollection.asp adresinde örnek
@@ -111,18 +111,18 @@ const dbName = "hellomongo";
 
 // Sunucuya bağlanma
 MongoClient.connect(mongoDbServer, function (err, client) {
-  if (err) throw err;
-  const db = client.db(dbName);
+    if (err) throw err;
+    const db = client.db(dbName);
 
-  console.log("Bağlantı başarıyla kuruldu");
+    console.log("Bağlantı başarıyla kuruldu");
 
-  insertDocuments(db, function () {
-    updateDocument(db, function () {
-      deleteDocument(db, function () {
-        findDocuments(db, function () {
-          client.close();
+    insertDocuments(db, function () {
+        updateDocument(db, function () {
+            deleteDocument(db, function () {
+                findDocuments(db, function () {
+                    client.close();
+                });
+            });
         });
-      });
     });
-  });
 }); //YAZMA İŞLEMİ BİTİNCE TERMİNAL E EN ÜSTTEKİ EXPRESS İŞLEMLERİNİ  GİRDİKTEN SONRA NODE İNDEX.JS YAZARAK KODUMUZU ÇALIŞTIRIYORUZ
